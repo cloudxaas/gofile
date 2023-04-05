fa2b39cfad9e17e690dbffc41d0cf9b4419e669a
Kol Influence <104323920+kolinfluence@users.noreply.github.com>
2023-04-05 14:51:48 -0700

    Update json.go

--- json/json.go
+++ json/json.go
@@ -1,29 +1,28 @@
 package cxfilejson
 
-/*
+
 import (
   "github.com/valyala/fastjson"
 	"io/ioutil"
 	"os"
 )
 
-func ParseJsonFile(file string) (*Config, error) {
+func ParseJsonFile(file string, config *Config) error {
 	f, err := os.Open(file)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer f.Close()
 
 	data, err := ioutil.ReadAll(f)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	config := &Config{}
-	if err := fastjson.Unmarshal(data, config); err != nil {
-		return nil, err
+	//config := &Config{}
+	if err := fastjson.Unmarshal(data, &config); err != nil {
+		return err
 	}
 
-	return config, nil
+	return nil
 }
-*/
